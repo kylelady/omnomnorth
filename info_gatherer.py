c984041ef5bef68cd172c938a215915b872279d2
Kyle Lady <kylelady@umich.edu>
2012-08-11 19:59:41 +0000

    Initial Commit!

---
+++ info_gatherer.py
@@ -0,0 +1,53 @@
+#/usr/bin/env python
+
+from datetime import datetime
+
+import places
+import timezones
+
+class InfoGatherer(object):
+    _places = {
+                'oncampus': {
+                    'commons': places.CommonsCafe(),
+                    'mujo': places.Mujo(),
+                    'panda': places.PandaExpress(),
+                    'quiznos': places.Quiznos(),
+                    'ugos': places.Ugos()
+                }, 'prfe': {
+                    'bagelfragel': places.BagelFragel(),
+                    'biggby': places.Biggby(),
+                    'greatplains': places.GreatPlains(),
+                    'lucky': places.LuckyKitchen(),
+                    'nothai': places.NoThai(),
+                    'qdoba': places.Qdoba(),
+                    'saica': places.Saica(),
+                    'seoulst': places.SeoulStreet(),
+                    'subway': places.CourtyardSubway(),
+                    'syrianbakery': places.SyrianBakery()
+                }, 'krogerville': {
+                    'espresso': places.EspressoRoyale(),
+                    'jj': places.JimmyJohns(),
+                    'kroger': places.Kroger(),
+                    'zoup': places.Zoup()
+                }, 'plymouth': {
+                    'buschs': places.Buschs(),
+                    'olgas': places.Olgas(),
+                    'sweetwaters': places.Sweetwaters()
+                }
+    }
+
+    def get_statuses(self):
+        res = {}
+        now = datetime.now()
+        for area, places in self._places.iteritems():
+            res[area] = []
+            for name, place in places.iteritems():
+                res[area].append({'id': name,
+                        'status':place.get_status(datetime.now(timezones.Eastern))})
+        return res
+
+if __name__ == '__main__':
+    p = places.PandaExpress()
+    print 'Hi!'
+    ig = InfoGatherer()
+    print ig.get_statuses()
---
+++ places/__init__.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from restaurant import State
+
+from commons import CommonsCafe
+from mujo import Mujo
+from panda import PandaExpress
+from quiznos import Quiznos
+from ugos import Ugos
+
+from bagelfragel import BagelFragel
+from biggby import Biggby
+from great_plains import GreatPlains
+from lucky import LuckyKitchen
+from nothai import NoThai
+from qdoba import Qdoba
+from saica import Saica
+from seoul import SeoulStreet
+from subway import CourtyardSubway
+from syria import SyrianBakery
+
+from espresso_royale import EspressoRoyale
+from jj import JimmyJohns
+from kroger import Kroger
+from zoup import Zoup
+
+from buschs import Buschs
+from olgas import Olgas
+from sweetwaters import Sweetwaters
